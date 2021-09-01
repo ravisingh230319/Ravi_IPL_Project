e55dd8f55899758328a7aa0276a5af8d55c6b925
Ravi Kumar Singh <ravi.singh.17@mountblue.tech>
2021-09-01 15:47:36 +0530

    Updated Higher Order Functions using chainimg

--- src/server/ipl.js
+++ src/server/ipl.js
@@ -2,15 +2,13 @@
 function totalMatchesPlayedPerYear(matches) {
     let results = {};
 
-    matches.forEach(match => {
-        const season=match.season;
-        if (season in results){
-            results[season] += 1;
-        }
-        else{
-            results[season] = 1;
-        }
-    });
+    results = matches
+        .map((match) => match.season)
+        .reduce((season, year) => {
+            year in season ? season[year] += 1 : season[year] = 1;
+            return season;
+        }, {});
+
     return results;
 }
 
@@ -19,39 +17,44 @@ function noOfMatchesWonPerTeamPerYear(matches) {
     let results = {};
     let matchesWonPerTeam = {};
 
-    matches.forEach((match) => {
-        if (!results.hasOwnProperty(match.season)) {
-            matchesWonPerTeam = {};
-        } else {
-            if (matchesWonPerTeam.hasOwnProperty(match.winner)) {
-                matchesWonPerTeam[match.winner] += 1;
-            } else if (match.winner != "") {
-                matchesWonPerTeam[match.winner] = 1;
+    matches
+        .filter((match) => match.winner != "")
+        .forEach((match) => {
+            if (!results.hasOwnProperty(match.season)) {
+                matchesWonPerTeam = {};
+            } else {
+                if (matchesWonPerTeam.hasOwnProperty(match.winner)) {
+                    matchesWonPerTeam[match.winner] += 1;
+                } else {
+                    matchesWonPerTeam[match.winner] = 1;
+                }
             }
-        }
-        results[match.season] = matchesWonPerTeam;
-    });
+            results[match.season] = matchesWonPerTeam;
+        });
+                  
     return results;
 }
 
 //This function used to count the extra runs conceded per team in the year 2016.
 function extraRunsConcededPerTeam2016(deliveries, matches) {
     let results = {};
-    let seasonDetails = [];
 
-    seasonDetails = matches.filter(match => match.season == 2016);
-    seasonDetails.forEach((matchId) => {
-        deliveries.forEach((delivery) => {
-            if (delivery.match_id == matchId.id) {
-                if (!results.hasOwnProperty(delivery.bowling_team)) {
-                    results[delivery.bowling_team] = Number(delivery.extra_runs);
-                } else {
-                    results[delivery.bowling_team] += Number(delivery.extra_runs);
+    matches
+        .filter(match => match.season == 2016)
+        .map(match=>match.id)
+        .forEach(matchId=>{
+            deliveries
+            .forEach((delivery) => {
+                if (delivery.match_id == matchId) {
+                    if (delivery.bowling_team in results) {
+                        results[delivery.bowling_team] += Number(delivery.extra_runs);
+                    } else {
+                        results[delivery.bowling_team] = Number(delivery.extra_runs);
+                    }
                 }
-            }
-        });
-    });
-
+            });
+        })   
+    
     return results;
 }
 
@@ -60,47 +63,48 @@ function top10EconomicalBowlers2015(deliveries, matches) {
     let results = {};
     let economyOfBowler = {};
     let ball = {};
-    let seasonDetails = [];
 
-    seasonDetails = matches.filter(match => match.season == 2015);
-    seasonDetails.forEach((matchId) => { 
-        deliveries.forEach((delivery) => { 
-            let wideBall = delivery.wide_runs;
-            let noBall = delivery.noball_runs;
+    matches
+        .filter((match) => match.season == 2015)
+        .map((match) => match.id)
+        .forEach((matchId) => {
+            deliveries.forEach((delivery) => {
+                let wideBall = delivery.wide_runs;
+                let noBall = delivery.noball_runs;
 
-            if (delivery.match_id == matchId.id) {
-                if (!economyOfBowler.hasOwnProperty(delivery.bowler)) {
-                    economyOfBowler[delivery.bowler] = Number(delivery.total_runs);
-                } else {
-                    economyOfBowler[delivery.bowler] += Number(delivery.total_runs);
-                }
-
-                if (!ball.hasOwnProperty(delivery.bowler)) {
-                    if (wideBall == 0 && noBall == 0) {
-                        ball[delivery.bowler] = 1;
+                if (delivery.match_id == matchId) {
+                    if (delivery.bowler in economyOfBowler) {
+                        economyOfBowler[delivery.bowler] += Number(delivery.total_runs);
                     } else {
-                        ball[delivery.bowler] = 0;
+                        economyOfBowler[delivery.bowler] = Number(delivery.total_runs);
                     }
-                } else {
-                    if (wideBall == 0 && noBall == 0) {
-                        ball[delivery.bowler] += 1;
+
+                    if (!ball.hasOwnProperty(delivery.bowler)) {
+                        if (wideBall == 0 && noBall == 0) {
+                            ball[delivery.bowler] = 1;
+                        } else {
+                            ball[delivery.bowler] = 0;
+                        }
+                    } else {
+                        if (wideBall == 0 && noBall == 0) {
+                            ball[delivery.bowler] += 1;
+                        }
                     }
                 }
-            }
+            });
         });
-    });
 
     let sortedEconomy = ball;
 
-    Object.keys(sortedEconomy).map(key => {
+    Object.keys(sortedEconomy)
+        .map(key => {
         sortedEconomy[key] = ((economyOfBowler[key] * 6) / sortedEconomy[key]).toFixed(2);
-    });
-    // In sorting the below line puts all the NaN values in order after sorting
-    sortedEconomy = Object.fromEntries(Object.entries(sortedEconomy).sort((x, y) => x[1] - y[1]));    
-    results = Object.keys(sortedEconomy).slice(0, 10).reduce((result, key) => {
-        result[key] = sortedEconomy[key];
-        return result;
-    }, {});
+        });
+        
+    results = Object.fromEntries(Object.entries(sortedEconomy)
+        .sort((x, y) => x[1] - y[1])
+        .slice(0,10));    
+    
     return results;
 }
 
